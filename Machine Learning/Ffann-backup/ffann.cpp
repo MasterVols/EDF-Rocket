eb79c428a13bd7ed9e4f44e250d316867df10a4f
PhillipRGraham <113307102+PhillipRGraham@users.noreply.github.com>
2023-04-11 00:27:34 -0400

    I created a function version of my network.
    I completely remade my network to no longer use symnolic classes for neurons etc and rather to just use arrays.

--- Machine Learning/Ffann-backup/ffann.cpp
+++ Machine Learning/Ffann-backup/ffann.cpp
@@ -1,7 +1,7 @@
 // ffann.cpp
 // Contributor: Phillip Graham
 // Time Started: March, 05, 2023
-// Last Edited: April, 08, 2023
+// Last Edited: April, 10, 2023
 // Description: This file is made to act as the main file for describing
 //              our objecy ffann. This object is used to implement a
 //              neuralnetwork of my design. This file will be used by my
@@ -9,167 +9,128 @@
 //              in this file to create a neural network. It will then
 //              output these networks to the user.
 
-
 using namespace std;
 #include <string>
 #include <vector>
 #include "ffann.hpp"
 #include <sstream>
+#include <iostream>
 
-synapse::synapse(int weight_param)
-{
-	post_neuron = NULL;
-	weight = weight_param;
-}
+#include <vector>
+#include <string> 
 
-synapse::~synapse()
-{
-	if (post_neuron != NULL)
-	{
-		delete post_neuron;
-	}
-}
+using namespace std;
 
-// Propigation will involve a synapse reading in a spike parameter, and then propigating
-// that spike to its post-neuron after applying its weight to the spike
-bool synapse::propigate(int spike_param)
-{
-	spike_param = spike_param * weight;
-	return this->post_neuron->set_and_fire(spike_param);
-}
-
-void synapse::set_post (neuron* post_param)
-{
-	this->post_neuron = post_param;
-}
-
-neuron::neuron(int threshold_param)
-{
-	threshold = threshold_param;
-	state = 0;
-}
-
-neuron::~neuron()
-{
-	for (size_t i = 0; i < outputs.size(); i++)
-	{
-		if (outputs[i] != NULL) delete outputs[i];
-	}
-}
-
-bool neuron::set_and_fire(int state_param)
-{
-	state += state_param;
-	if (state >= threshold)
-	{
-		for (size_t i = 0; i < outputs.size(); i++) outputs[i]->propigate(state);
-		state = 0;
-		return true;
-	}
-	return false;
-}
 
 ffann::ffann (int num_input_param,int num_output_param,int num_hidden_layer_param,string network_param)
 {
-        num_input = num_input_param;
-		num_output = num_output_param;
-        num_hidden_layer = num_hidden_layer_param;
-        num_neurons = num_input + num_output + (num_input + num_output) * num_hidden_layer_param;
-		network.resize(num_neurons);
-		generate_network(network_param);
+    num_input = num_input_param;
+	num_output = num_output_param;
+    num_hidden_layer = num_hidden_layer_param;
+    num_neurons = num_input + num_output + (num_input + num_output) * num_hidden_layer_param;
+    states.resize(2+num_hidden_layer);
+    weights.resize(2+num_hidden_layer);
+    thresholds.resize(2+num_hidden_layer);
+
+    istringstream istr (network_param);
+    for (int i = 0; i < (2 + num_hidden_layer); i++)
+    {
+        int slice_height;
+        int post_per_node;
+
+        if (i == 0)
+        {
+            slice_height = num_input;
+            post_per_node = num_input + num_output;
+        }
+        else if (i < num_hidden_layer + 1)
+        {
+            slice_height = num_input + num_output;
+            if (i == num_hidden_layer) post_per_node = num_output;
+            else post_per_node = num_output + num_input;
+        }
+        else
+        {
+            slice_height = num_output;
+            post_per_node = 0;
+        }
+
+        thresholds[i].resize(slice_height,0);
+        states[i].resize(slice_height,0);
+        weights[i].resize(slice_height);
+        for (int j = 0; j < slice_height; j++)
+        {
+            weights[i][j].resize(post_per_node,0);
+        }
+        
+
+        for (int j = 0; j < slice_height; j++)
+        {
+            int thresh_val;
+            istr >> thresh_val;
+            thresholds[i][j] = thresh_val;
+            for (int k = 0; k < post_per_node; k++)
+            {
+                int weight_val;
+                istr >> weight_val;
+                weights[i][j][k] = weight_val;
+            }
+        }
+    }
 }
 
-ffann::~ffann ()
+vector <int> ffann::evaluate_network(vector<int> inp_param)
 {
-	for (size_t i = 0; i < num_input; i++) delete network[i];
+    vector <int> out_vect;
+    
+    for (int i = 0; i < num_input; i++)
+    {
+        states[0][i] = inp_param[i];
+    }
+    
+    // Propigate the signals
+    for (unsigned int i = 0; i < states.size() - 1; i++)
+    {
+        for (unsigned int j = 0; j < states[i].size(); j++)
+        {
+            if (states[i][j] >= thresholds[i][j])
+            {
+                for (unsigned int k = 0; k < weights[i][j].size(); k++)
+                {
+                    states[i+1][k] += states[i][j] * weights[i][j][k];
+                }
+            }
+        }
+        for (unsigned int j = 0; j < states[i].size(); j++)
+        {
+            states[i][j] = 0;
+        }
+    }
+    
+    for (int i = 0; i < num_output; i++)
+    {
+        out_vect.push_back(states[num_hidden_layer + 1][i]);
+        states[num_hidden_layer + 1][i] = 0;
+    }
+    
+    
+
+    return out_vect;
 }
 
-void ffann::generate_network (string net_param)
+void ffann::print_network()
 {
-	istringstream istr;
-	istr.str(net_param);
-	for (int i = 0; i < num_neurons; i++)
-	{
-		if (i < num_input) // If we are reading in input
-		{
-			int threshold;
-			istr >> threshold;
-			neuron* n_ptr = new neuron (threshold);
-			for (int j = 0; j < num_input + num_output; j++)
-			{
-				int weight;
-				istr >> weight;
-				synapse* s_ptr = new synapse(weight);
-				n_ptr->outputs[j] = s_ptr;
-			}
-			network.push_back(n_ptr);
-		}
-		else if (i < (num_neurons - num_output)) // If we are reading in a hidden layer
-		{
-			int threshold;
-			istr >> threshold;
-			neuron* n_ptr = new neuron (threshold);
-			if (num_input <= i && (i < (num_input * 2 + num_output)))
-			{
-				for (int j = 0; j < num_input; j++)
-				{
-					network[j]->outputs[i % num_input]->set_post(n_ptr);
-				}
-			}
-			else
-			{
-				for (int j = 0; j < num_input + num_output; j++)
-				{
-					network[i - num_input - num_output + j]->outputs[(i - num_input) % (num_input + num_output)]->set_post(n_ptr);
-				}
-			}
-
-			if (i + num_input + num_output < (num_neurons - num_output))
-			{
-				for (int j = 0; j < num_input + num_output; j++)
-				{
-					int weight;
-					istr >> weight;
-					synapse* s_ptr = new synapse(weight);
-					n_ptr->outputs[j] = s_ptr;
-				}
-			}
-			else
-			{
-				for (int j = 0; j < num_output; j++)
-				{
-					int weight;
-					istr >> weight;
-					synapse* s_ptr = new synapse(weight);
-					n_ptr->outputs[j] = s_ptr;
-				}
-			}
-		}
-		else // If we are reading in an output layer
-		{
-			int threshold;
-			istr >> threshold;
-			neuron* n_ptr = new neuron (threshold);
-			n_ptr->outputs.resize(0);
-			for (int j = 0; j < num_input + num_output; j++)
-			{
-				network[network.size() - (num_output * 2) - num_input + j]->outputs[i - (num_input + (num_input + num_output) * num_hidden_layer)]->set_post(n_ptr);
-			}
-		}
-	}
-}
-
-vector <int> ffann::evaluate_network (vector <int> input_vector_param)
-{
-    vector <int> evaluation_vector;
-	for (size_t i = 0; i < input_vector_param.size(); i++)
-	{
-		network[i]->set_and_fire(input_vector_param[i]);
-	}
-
-	for (int i = 0; i < num_output; i++)
-	{
-		evaluation_vector.push_back(network[num_neurons - num_output + i]->state);
-	}	
-	return evaluation_vector;
+    for (int i = 0; i < num_hidden_layer + 2; i++)
+    {
+        for (unsigned int j = 0; j < states[i].size(); j++)
+        {
+            cout << "S=" << states[i][j] << " T=" << thresholds[i][j];
+            for (unsigned int k = 0; k < weights[i][j].size();k++)
+            {
+                cout << " W=" << weights[i][j][k];
+            }
+            if (j != states[i].size() - 1) cout << "___";
+        }
+        if (i != states.size() - 1) cout << "\n";
+    }
 }
--- Machine Learning/Ffann-backup/ffann.hpp
+++ Machine Learning/Ffann-backup/ffann.hpp
@@ -1,7 +1,7 @@
 // ffann.hpp
 // Contributro: Phillip Graham
 // Time Started: March, 27, 2023
-// Last Edited: April, 08, 2023
+// Last Edited: April, 11, 2023
 // Description: This file contains the initilization of the classes which will be used 
 // 		for my implementation of a feed forward, artificial, neural network.
 // 		It will initialize our neuron, synapse, and ffann classes.
@@ -11,44 +11,16 @@
 
 using namespace std;
 
-class neuron;
-
-class synapse
-{
-	friend class neuron;
-	public:
-		synapse(int);
-		~synapse();
-		bool propigate(int);
-		void set_post(neuron*);
-	private:
-		int weight;
-		neuron * post_neuron;
-};
-
-class neuron
-{
-	friend class ffann;
-	public:
-		neuron(int);
-		~neuron();
-		bool set_and_fire (int);
-		void generate_post_synapse (vector<int>);
-	private:
-		int threshold;
-		int state;					// The state represents the accumulation of a neurons inputs
-		vector <synapse*> outputs;
-};
-
 class ffann
 {
 	public:
 		ffann(int,int,int,string);
-		~ffann();
 		vector<int> evaluate_network(vector<int>);
-		void generate_network (string);
+		void print_network ();
 	private:
-		vector<neuron*> network;
+		vector<vector<int>> states;
+		vector<vector<int>> thresholds;
+		vector<vector<vector<int>>> weights;
 		int num_input;
 		int num_output;
 		int num_hidden_layer;
